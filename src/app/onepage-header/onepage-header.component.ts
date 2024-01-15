d4ba2ce919e2e88b6f9c44aed5c1b2f26c774a51
Hamit Mizrak <hamitmizrak@gmail.com>
2024-01-15 11:43:34 +0300

    Navbar

--- src/app/onepage-header/onepage-header.component.ts
+++ src/app/onepage-header/onepage-header.component.ts
@@ -22,10 +22,29 @@ export class OnepageHeaderComponent {
 
   homePage: String = 'Anasayfa 44';
 
-  // isLogin: boolean = true;
-
   // navBar adında bir obje oluşturunuz.
   // Logo(string), link(string), search, attribute oluşturuyorsunuz.
   // Link (dizi)
+  // navBar:{}={};
+  navBar:{
+    isLogin:boolean,
+    navbar:Array<any>,
+    search:{}
+  }={
+    isLogin:true,
+    //Anasayfa,AboutMe,Blog,Contact  name: Anasayfa, link: http://, fontAwesome
+    navbar:[
+      {id:1,name:"Anasayfa",link:"/",fontAwesome:"fa-solid fa-house-chimney"},
+      {id:2,name:"AboutMe",link:"/about-me",fontAwesome:"font-2"},
+      {id:3,name:"Blog",link:"/blog",fontAwesome:"font-3"},
+      {id:4,name:"Contact",link:"/contact",fontAwesome:"font-4"},
+     ],  
+    search:{
+      name:"Search",
+      placeholder:"Search",
+      type:"text",
+    }
+  }; //navBar Object
 
-}
+
+} //end OnepageHeaderComponent
