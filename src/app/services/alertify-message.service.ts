48916b4ca80cfe7db6a9644d8ab25503f343efa4
Hamit Mizrak <hamitmizrak@gmail.com>
2024-01-22 11:30:23 +0300

    Alertify Service

--- src/app/services/alertify-message.service.ts
+++ src/app/services/alertify-message.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AlertifyMessageService {
+// Alertify
+declare let alertify: any;
 
-  constructor() { }
-}
+// Global State veya Local Service State için yazmalısınız
+// @Injectable({
+//   providedIn: 'root' //providedIn: Kaldırırsam Local Service olacaktır.
+// })
+
+// GLOBAL_SERVICE
+@Injectable({
+  providedIn: 'root',
+})
+
+//EXPORT
+export class AlertifyMessageService {
+  // Constructor
+  constructor() {}
+
+  // Success
+  alertSuccess(message: string) {
+    alertify.success(message);
+  }
+
+  // Warning
+  alertWarning(message: string) {
+    alertify.warning(message);
+  }
+
+  // Message
+  alertMessage(message: string) {
+    alertify.message(message);
+  }
+
+  // Error
+  alertError(message: string) {
+    alertify.error(message);
+  }
+} //end AlertifyMessageService
--- src/app/services/blog-array.service.ts
+++ src/app/services/blog-array.service.ts
@@ -1,9 +1,19 @@
+// CORE
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+// Global State veya Local Service State için yazmalısınız
+// @Injectable({
+//   providedIn: 'root' //providedIn: Kaldırırsam Local Service olacaktır.
+// })
+
+// LOCAL SERVICE
+@Injectable()
+
+// EXPORT
 export class BlogArrayService {
 
+  // Constructor
   constructor() { }
-}
+
+} //end BlogArrayService
